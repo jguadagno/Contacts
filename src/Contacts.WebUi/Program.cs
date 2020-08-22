9c2db80fd1170be5f32b02a940415aa3b09d9fc1
jguadagno <jguadagno@hotmail.com>
2020-08-21 17:18:32 -0700

    Added the loading of user secrets.

--- src/Contacts.WebUi/Program.cs
+++ src/Contacts.WebUi/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
@@ -31,6 +32,13 @@ namespace Contacts.WebUi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, builder) =>
+                {
+                    if (hostContext.HostingEnvironment.IsDevelopment())
+                    {
+                        builder.AddUserSecrets<Program>();
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- src/Contacts.WebUi/Startup.cs
+++ src/Contacts.WebUi/Startup.cs
@@ -87,8 +87,6 @@ namespace Contacts.WebUi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
-            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
